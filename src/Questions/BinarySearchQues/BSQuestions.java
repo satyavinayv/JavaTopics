e6a3e8711f9cce6edb46b7662d48fe2abb949353
SatyaVinayV <manikantasai811@gmail.com>
2026-08-15 21:08:35 +0530

    BS Q1

---
+++ src/Questions/BinarySearchQues/BSQuestions.java
@@ -0,0 +1,50 @@
+package Questions.BinarySearchQues;
+
+public class BSQuestions {
+    public static void main(String[] args) {
+
+    }
+
+    // Find First and Last Position of Element in Sorted Array
+    /*
+        Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+        If target is not found in the array, return [-1, -1].
+        You must write an algorithm with O(log n) runtime complexity.
+     */
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] ans = {-1, -1};
+        // check for first occurrence if target first
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+    }
+
+    // this function just returns the index value of target
+    int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // potential answer found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
