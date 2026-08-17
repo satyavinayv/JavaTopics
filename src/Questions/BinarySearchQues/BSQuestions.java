e8a16ff1ff0822a9cb0e521208233bc56498ec3c
SatyaVinayV <manikantasai811@gmail.com>
2026-08-17 07:59:20 +0530

    BS Questions

--- src/Questions/BinarySearchQues/BSQuestions.java
+++ src/Questions/BinarySearchQues/BSQuestions.java
@@ -47,4 +47,88 @@ public class BSQuestions {
         }
         return ans;
     }
+
+    // Search of ele in infinite array
+    public static int ans(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+        while (arr[end] < target) {
+            int newStart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+        return binarySearchInfiniteArray(arr, target, start, end);
+    }
+
+    public static int binarySearchInfiniteArray(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                // Target is on the right
+                start = mid + 1;
+            } else {
+                // Target is on the left
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+    // Peak Index in a mountain array
+    public static int binarySearchInPeakArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < arr[mid + 1]) start = mid + 1;
+            else end = mid;
+        }
+        return start;
+    }
+
+    //1095. Find in Mountain Array
+    public static int findInMountainArray(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < arr[mid + 1]) start = mid + 1;
+            else end = mid;
+        }
+
+        int index1 = orderAgnosticBS(arr, target, 0, start);
+        if (index1 == -1) {
+            return orderAgnosticBS(arr, target, end + 1, arr.length - 1);
+        }
+        return index1;
+    }
+
+    static int orderAgnosticBS(int[] arr, int target, int start, int end) {
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
